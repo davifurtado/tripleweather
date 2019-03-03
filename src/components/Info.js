fdb16cd0ea54a38538fb61991ca61272c64937d3
davifurtado <44845457+davifurtado@users.noreply.github.com>
2019-03-03 13:24:52 -0300

    adding paragraphs on minMaxTemp

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,9 +7,18 @@ class Weather extends React.Component {
 				<p className="city">{ this.props.city }, { this.props.country }</p>
 				<p className="temp">{ this.props.temp } °C </p>
 				<div className="tempMaxMin">
-					<p>Max { this.props.temp_max } °C</p>
-					<p>Min { this.props.temp_min } °C</p>
-					<p>Umidade: { this.props.humidity }</p>   
+					<div>
+						<p>Max</p>
+						<p>{ this.props.temp_max } °C</p>
+					</div>
+					<div>
+						<p>Min</p> 
+						<p>{ this.props.temp_min } °C</p>
+					</div>
+					<div>
+						<p>Umidade</p> 
+						<p>{ this.props.humidity }</p>
+					</div>
 				</div>     		
 			</div>
 		);
