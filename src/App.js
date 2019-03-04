0b7b0740c4b2304ce3bb4668175e0b7d66903c8e
davifurtado <44845457+davifurtado@users.noreply.github.com>
2019-03-03 22:50:10 -0300

    minor changes

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ class App extends React.Component {
     country: '',
     humidity: '',    
   }
-  getTime = async => {
-    
-  }
+
   getWeather = async (city) => {      
       const api_call_london =
        await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
